7bf9547663d87eb57456e979bc574861b2eff766
Felipe Torres <felipeost.torres@gmail.com>
2023-01-14 20:06:38 -0300

    fix send url_image

--- src/adapters/http/controllers/readImgController/readImgController.ts
+++ src/adapters/http/controllers/readImgController/readImgController.ts
@@ -3,15 +3,15 @@ import { IReadImg } from '../../../../useCases/readImgUseCase/readImgDTO'
 import { ReadImg } from '../../../../useCases/readImgUseCase/readImgUseCase'
 
 export class ReadImgController {
-  constructor (
+  constructor(
     private readImgUseCase: ReadImg
   ) { }
 
-  async handle (request: Request, response: Response): Promise<Response> {
-    const image = request.file.path
-    console.log(image)
+  async handle(request: Request, response: Response): Promise<Response> {
+    //const image = request.file.path
+    const { url_img } = request.body
     try {
-      const result = await this.readImgUseCase.execute(image)
+      const result = await this.readImgUseCase.execute(url_img)
       return response.status(200).json({ result })
     } catch (err) {
       return response.status(400).json({
