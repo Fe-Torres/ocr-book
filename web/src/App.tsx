6fd28571ee6f3754cc37b971877cd01d5788f475
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-01-20 12:08:12 -0300

    feat: add web login page structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import Home from "./pages/home.tsx"
+import Home from './pages/home'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App () {
   return (
-   <Home />  
+    <Home />
   )
 }
 
--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,9 +1,39 @@
+import { useState } from 'react'
+import { Button } from '../components/button'
+import { Input } from '../components/input'
+import { TitleWrapper } from '../components/title'
 
+export default function Home () {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-export default function Home() {
-    return (
-        <div className="max-w-full bg-red-100 flex items-center">
-            <p>teste</p>
+  return (
+    <div className="h-full p-8">
+      <div className="flex items-center flex-col">
+        <div className="flex items-center mt-12 flex-col gap-6">
+          <div className="flex justify-center">
+            <img
+              className="w-2/6 max-w-xs"
+              src="https://toppng.com/public/uploads/thumbnail/experience-the-discussion-online-library-book-logo-11562996058ip1or4eqvs.png"
+            />
+          </div>
+          <TitleWrapper>
+            <h1>CodeBookJS</h1>
+          </TitleWrapper>
         </div>
-    )
+        <form className='flex flex-col gap-4 items-center mt-12'>
+          <Input name='Email' onChange={(value) => setEmail(value)}/>
+          <Input name='Password' onChange={(value) => setPassword(value)}/>
+        </form>
+        <section className='w-full max-w-xs mt-8 flex flex-row justify-between items-center'>
+          <div className='w-1/4'>
+            <Button onClick={() => console.log('teste')}>Login</Button>
+          </div>
+          <a href="/teste" className='underline'>
+          How to use
+          </a>
+        </section>
+      </div>
+    </div>
+  )
 }
