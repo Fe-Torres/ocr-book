b104d3e992ac2a7a1f431c3d43bc59a18771b342
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-02-10 16:36:51 -0300

    style: change code result size

--- web/src/components/code-result.tsx
+++ web/src/components/code-result.tsx
@@ -4,9 +4,9 @@ type CodeResultProps = {
 
 export const CodeResult = ({ code }: CodeResultProps) => {
   return (
-    <div className="flex items-center flex-col mt-11">
-      <strong>Code result:</strong>
-      <p>{code}</p>
+    <div className="w-1/3 flex test-justify items-center flex-col mt-11">
+      <strong className="text-2xl">Code result:</strong>
+      <p className="text-xl mt-4">{code}</p>
     </div>
   )
 }
