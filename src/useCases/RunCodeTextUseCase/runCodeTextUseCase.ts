5e490df568db826f9c4506f8643a61ba1f056db0
Felipe Torres <felipeost.torres@gmail.com>
2023-08-03 02:37:16 -0300

    feat: adding structure to retry code

--- src/useCases/RunCodeTextUseCase/runCodeTextUseCase.ts
+++ src/useCases/RunCodeTextUseCase/runCodeTextUseCase.ts
@@ -13,25 +13,49 @@ export class RunCodeTextUseCase {
   }
 
   async execute(codeText: string): Promise<string> {
-    Logger.processMessage(
-      "RunCodeTextUseCase execute method",
-      ActionLog.INITIAL,
-      codeText
-    );
+    try {
+      Logger.processMessage(
+        "RunCodeTextUseCase execute method",
+        ActionLog.INITIAL,
+        codeText
+      );
+      const codeResult = await this.processCodeText(codeText);
+      Logger.processMessage("RunCodeTextUseCase execute method", ActionLog.END);
+      return codeResult;
+    } catch (error) {
+      const codeResultRetry = await this.retryRunCode(codeText, error);
+      return codeResultRetry;
+    }
+  }
+  private async retryRunCode(
+    codeWithError: string,
+    _error: any
+  ): Promise<string> {
+    // const fixedCode = this.codeFixer(codeWithError, error)
+    Logger.processMessage("retryRunCode method", ActionLog.INITIAL, {
+      codeWithError,
+      errorMessage: _error?.message,
+    });
+    // Logger.info(`Fix code: ${fixedCode}`);
+    const codeResultRetry = await this.processCodeText(codeWithError);
+    Logger.processMessage("retryRunCode method", ActionLog.INITIAL, {
+      codeWithError,
+      errorMessage: _error?.message,
+    });
+    return codeResultRetry;
+  }
+
+  private async processCodeText(codeText: string): Promise<string> {
+    Logger.processMessage("processMessage method", ActionLog.INITIAL, {
+      codeText,
+    });
     const resultMapped = this.mapperText.executeMapper(codeText);
     const codeTextModel = new CodeTextModel(resultMapped);
     await this.fileManager.createFile(codeTextModel.text);
     const codeResult = await this.fileManager.executeFile();
-
-    Logger.processMessage("RunCodeTextUseCase execute method", ActionLog.END);
-
+    Logger.processMessage("processMessage method", ActionLog.END, {
+      codeResult,
+    });
     return codeResult;
   }
-
-  // private async retryRunCode(error: string): Promise<string> {
-  //   const codeFixer = new CodeFixer(error, this.resultMapped);
-  //   this.resultMapped = await codeFixer.fixCode();
-  //   await this.createFile();
-  //   return;
-  // }
 }
