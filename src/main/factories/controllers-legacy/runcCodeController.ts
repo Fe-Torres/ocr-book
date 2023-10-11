23968edc23195744af6174f066a8e2ac7cd323e0
Felipe Torres <felipeost.torres@gmail.com>
2023-10-11 02:14:09 -0300

    feat: ok version - cansado dms pra comentar kkkkk

--- src/main/factories/controllers-legacy/runcCodeController.ts
+++ src/main/factories/controllers-legacy/runcCodeController.ts
@@ -1,4 +1,5 @@
 import { RunCodeTextController } from "../../../infra/api/controllers/runCodeTextController/runCodeTextController";
+import { ChatGptService } from "../../../infra/chatGptService/chatGpt";
 import { FileManager } from "../../../useCases/RunCodeTextUseCase/helpers/fileManager";
 import { MapperText } from "../../../useCases/RunCodeTextUseCase/helpers/mapperText";
 import { RunCodeTextUseCase } from "../../../useCases/RunCodeTextUseCase/runCodeTextUseCase";
@@ -6,7 +7,12 @@ import { RunCodeTextUseCase } from "../../../useCases/RunCodeTextUseCase/runCode
 export const makeRunCodeTextController = (): RunCodeTextController => {
   const mapperText = new MapperText();
   const fileManager = new FileManager();
-  const runCodeTextUseCase = new RunCodeTextUseCase(mapperText, fileManager);
+  const chatGptService = new ChatGptService();
+  const runCodeTextUseCase = new RunCodeTextUseCase(
+    mapperText,
+    fileManager,
+    chatGptService
+  );
   const runCodeTextController = new RunCodeTextController(runCodeTextUseCase);
   return runCodeTextController;
 };
